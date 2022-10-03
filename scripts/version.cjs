d61adf78f898c9e17cf3f0810150497b9bf39496
SonTrungTo <son.trung.to@gmail.com>
2022-10-03 06:40:25 +0300

    writing webpacks for ts-loader.

--- scripts/version.cjs
+++ scripts/version.cjs
@@ -1,1 +1,8 @@
 const fs = require("fs");
+const path = require("path");
+
+const packageVersion = require("../package.json").version;
+
+const content = `export const VERSION = "${packageVersion}";\n`;
+
+fs.writeFileSync(path.join(__dirname, '../source/shared/build.ts'), content);
--- source/shared/build.ts
+++ source/shared/build.ts
@@ -1,1 +1,1 @@
-const hello = 1;
+export const VERSION = "1.0.0";
